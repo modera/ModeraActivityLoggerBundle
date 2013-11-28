14568158b62c7cfd217ad5ab2f9b65e7b1e32610
Sergei Lissovski <sergei.lissovski@gmail.com>
2013-11-28 13:02:19 +0200

    now it is possible to exposer css resources when backend is loaded

--- Controller/IndexController.php
+++ Controller/IndexController.php
@@ -5,6 +5,7 @@ namespace Modera\SecurityAwareJSRuntimeBundle\Controller;
 use Modera\JSRuntimeIntegrationBundle\ClientSideDependencyInjection\ServiceDefinitionsManager;
 use Modera\JSRuntimeIntegrationBundle\DependencyInjection\ModeraJSRuntimeIntegrationExtension;
 use Modera\SecurityAwareJSRuntimeBundle\DependencyInjection\ModeraSecurityAwareJSRuntimeExtension;
+use Sli\ExpanderBundle\Ext\ContributorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -30,9 +31,13 @@ class IndexController extends Controller
         /* @var ServiceDefinitionsManager $definitionsMgr */
         $definitionsMgr = $this->container->get('mf.jsruntimeintegrationbundle.csdi.service_definitions_manager');
 
+        /* @var ContributorInterface $cssResourcesProvider */
+        $cssResourcesProvider = $this->get('mf.jsruntimeintegration.css_resources_provider');
+
         return [
             'config' => array_merge($runtimeConfig, $securedRuntimeConfig),
-            'container_services' => $definitionsMgr->getDefinitions()
+            'container_services' => $definitionsMgr->getDefinitions(),
+            'css_resources' => $cssResourcesProvider->getItems()
         ];
     }
 }
