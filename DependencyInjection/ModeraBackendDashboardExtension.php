1a1c890bcc2d2419b219f69805ce369c5e9b032e
Aleksandr Rudakov <alexandr.rudakov@modera.net>
2014-01-09 14:02:10 +0200

    Added service container tests

--- DependencyInjection/ModeraBackendDashboardExtension.php
+++ DependencyInjection/ModeraBackendDashboardExtension.php
@@ -20,9 +20,6 @@ class ModeraBackendDashboardExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
     }
