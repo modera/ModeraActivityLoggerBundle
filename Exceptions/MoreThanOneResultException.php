25a0720d8b842c3ac844b3765572ce0fbabdc4b7
Aleksandr Rudakov <alexandr.rudakov@modera.net>
2014-02-07 09:12:05 +0200

    Adding Nothing found exception if getAction, can not find any entity

---
+++ Exceptions/MoreThanOneResultException.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Modera\ServerCrudBundle\Exceptions;
+
+/**
+ * @author    Alex Rudakov <alexandr.rudakov@modera.org>
+ * @copyright 2014 Modera Foundation
+ */
+class MoreThanOneResultException extends BadRequestException
+{}
