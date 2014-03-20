225ce5f140d36f8eac3686a9b4e6a294645285e7
Sergei Lissovski <sergei.lissovski@gmail.com>
2014-03-20 11:28:13 +0200

    Util/ControllerServiceNameFormatter removed

--- Controller/AbstractBaseController.php
+++ Controller/AbstractBaseController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  * Controller provides a bunch of auxiliary methods.
  *
  * @copyright 2013 Modera Foundation
- * @author Sergei Lissovski <sergei.lissovski@modera.net>
+ * @author Sergei Lissovski <sergei.lissovski@modera.org>
  */
 class AbstractBaseController extends Controller
 {
@@ -30,29 +30,5 @@ class AbstractBaseController extends Controller
     protected function sc()
     {
         return $this->get('security.context');
-    }
-
-    /**
-     * @return string
-     */
-    private function getServicePrefix()
-    {
-        /* @var \Modera\FoundationBundle\Util\ControllerServiceNameFormatter $formatter */
-        $formatter = $this->get('mf.foundation.util.controller_service_name_formatter');
-
-        return $formatter->formatPrefix(get_class($this));
-    }
-
-    /**
-     * Provides an easy way to access container services defined for this bundle.
-     *
-     * For a controller placed in MyCompany/Bundle/FooBundle or MyCompany/FooBundle for $id "my_service"
-     * will return "mycompany.foo.my_service".
-     *
-     * @return Object
-     */
-    protected function service($id)
-    {
-        return $this->get($this->getServicePrefix().'.'.$id);
     }
 }
