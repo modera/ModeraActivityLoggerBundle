29da29a5e260858965b9343136323438581d16f5
Sergei Lissovski <sergei.lissovski@gmail.com>
2014-04-26 14:21:58 +0300

    [MPFE-531] all english phrases now are translatable; KernelConfigWriter added

--- Contributions/ConfigEntriesProvider.php
+++ Contributions/ConfigEntriesProvider.php
@@ -21,23 +21,35 @@ class ConfigEntriesProvider implements ContributorInterface
         $yes = T::trans('yes');
         $no = T::trans('no');
 
-        $kernelDebug = array(
-            'id' => 'modera_config.boolean_handler',
+        $kernelDebugServer = array(
+            'handler' => 'modera_config.boolean_handler',
             'true_text' => $yes,
-            'false_text' => $no
+            'false_text' => $no,
+
+            'update_handler' => 'modera_dynamically_configurable_app.value_handling.kernel_config_writer'
+        );
+        $kernelDebugClient = array(
+            'xtype' => 'combo',
+            'store' => [['prod', 'yes'], ['dev', 'no']]
         );
 
-        $kernelEnv = array(
-            'id' => 'modera_config.dictionary_handler',
+        $kernelEnvServer = array(
+            'handler' => 'modera_config.dictionary_handler',
             'dictionary' => array(
                 'prod' => $yes,
                 'dev' => $no
-            )
+            ),
+
+            'update_handler' => 'modera_dynamically_configurable_app.value_handling.kernel_config_writer',
+        );
+        $kernelEnvClient = array(
+            'xtype' => 'combo',
+            'store' => [[true, 'yes'], [false, 'no']]
         );
 
         return array(
-            new CED(Bundle::CONFIG_KERNEL_ENV, 'Production mode', 'prod', $kernelEnv),
-            new CED(Bundle::CONFIG_KERNEL_DEBUG, 'Maintenance mode', false, $kernelDebug)
+            new CED(Bundle::CONFIG_KERNEL_ENV, T::trans('Production mode'), 'prod', 'general', $kernelEnvServer, $kernelDebugClient),
+            new CED(Bundle::CONFIG_KERNEL_DEBUG, T::trans('Maintenance mode'), false, 'general', $kernelDebugServer, $kernelEnvClient)
         );
     }
 }
