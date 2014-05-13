efd9cd00d77d4abf14b21b37ed73900db7a118a8
Sergei Lissovski <sergei.lissovski@gmail.com>
2014-05-13 10:24:49 +0300

    [MPFE-567] ModeraJSRuntimeIntegrationExtension renamed to ModeraMjrIntegrationExtension

--- Contributions/CssResourcesProvider.php
+++ Contributions/CssResourcesProvider.php
@@ -2,7 +2,7 @@
 
 namespace Modera\MJRThemeIntegrationBundle\Contributions;
 
-use Modera\JSRuntimeIntegrationBundle\DependencyInjection\ModeraJSRuntimeIntegrationExtension;
+use Modera\MjrIntegrationBundle\DependencyInjection\ModeraMjrIntegrationExtension;
 use Modera\MJRThemeIntegrationBundle\DependencyInjection\ModeraMJRThemeIntegrationExtension;
 use Modera\SecurityAwareJSRuntimeBundle\DependencyInjection\ModeraSecurityAwareJSRuntimeExtension;
 use Sli\ExpanderBundle\Ext\ContributorInterface;
@@ -22,7 +22,7 @@ class CssResourcesProvider implements ContributorInterface
     public function __construct(ContainerInterface $container)
     {
         $this->themeIntegrationConfig = $container->getParameter(ModeraMJRThemeIntegrationExtension::CONFIG_KEY);
-        $this->mjrInteggrationConfig = $container->getParameter(ModeraJSRuntimeIntegrationExtension::CONFIG_KEY);
+        $this->mjrInteggrationConfig = $container->getParameter(ModeraMjrIntegrationExtension::CONFIG_KEY);
     }
 
     /**
