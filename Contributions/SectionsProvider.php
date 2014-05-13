5082e7130ccf137ab59c4fdf2b2d9e440c56974d
Sergei Lissovski <sergei.lissovski@gmail.com>
2014-05-13 09:54:57 +0300

    [MPFE-567] renaming js-runtime to mjr-integration

--- Contributions/SectionsProvider.php
+++ Contributions/SectionsProvider.php
@@ -2,7 +2,7 @@
 
 namespace Modera\BackendSecurityBundle\Contributions;
 
-use Modera\JSRuntimeIntegrationBundle\Sections\Section;
+use Modera\MjrIntegrationBundle\Sections\Section;
 use Sli\ExpanderBundle\Ext\ContributorInterface;
 
 /**
