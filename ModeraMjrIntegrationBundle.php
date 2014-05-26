08ed4a0a160f3b85c76f8422993eb9f60df0e6ef
Sergei Lissovski <sergei.lissovski@gmail.com>
2014-05-26 15:25:27 +0300

    [MPFE-609] bundle updated to leverage latest extension-points architecture

--- ModeraMjrIntegrationBundle.php
+++ ModeraMjrIntegrationBundle.php
@@ -4,6 +4,7 @@ namespace Modera\MjrIntegrationBundle;
 
 use Modera\MjrIntegrationBundle\DependencyInjection\MenuItemContributorCompilerPass;
 use Sli\ExpanderBundle\DependencyInjection\CompositeContributorsProviderCompilerPass;
+use Sli\ExpanderBundle\Ext\ExtensionPoint;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -20,48 +21,39 @@ class ModeraMjrIntegrationBundle extends Bundle
      */
     public function build(ContainerBuilder $container)
     {
-        // lets to contribute implementations of \Modera\MjrIntegrationBundle\Config\ConfigMergerInterface
-        // that will be used to prepare runtime-config
-        $container->addCompilerPass(
-            new CompositeContributorsProviderCompilerPass('modera_mjr_integration.config.config_mergers_provider')
+        $configMergersProvider = new ExtensionPoint('modera_mjr_integration.config.config_mergers_provider');
+        $configMergersProvider->setDescription(
+            'Lets to contribute implementations of ConfigMergerInterface that will be used to prepare runtime-config.'
         );
+        $container->addCompilerPass($configMergersProvider->createCompilerPass());
 
         // allows to contribute menu items by defining them in config file
         $container->addCompilerPass(new MenuItemContributorCompilerPass());
 
-        // allows to contribute sections that will be displayed in backend menu
-        $container->addCompilerPass(
-            new CompositeContributorsProviderCompilerPass('modera_mjr_integration.menu.menu_items_provider')
-        );
+        $menuItemsProvider = new ExtensionPoint('modera_mjr_integration.menu.menu_items_provider');
+        $menuItemsProvider->setDescription('Allows to contribute sections that will be displayed in backend menu.');
+        $container->addCompilerPass($menuItemsProvider->createCompilerPass());
 
-        // client side dependency injection container services providers
-        $container->addCompilerPass(
-            new CompositeContributorsProviderCompilerPass('modera_mjr_integration.csdi.service_definitions_provider')
-        );
+        $serviceDefinitionsProvider = new ExtensionPoint('modera_mjr_integration.csdi.service_definitions_provider');
+        $serviceDefinitionsProvider->setDescription('client side dependency injection container services providers.');
+        $container->addCompilerPass($serviceDefinitionsProvider->createCompilerPass());
 
-        // contributes section which will be possible to activate ( see js class MF.runtime.Section )
-        $container->addCompilerPass(
-            new CompositeContributorsProviderCompilerPass('modera_mjr_integration.sections_provider')
-        );
+        $sectionsProvider = new ExtensionPoint('modera_mjr_integration.sections_provider');
+        $sectionsProvider->setDescription('Contributes section which will be possible to activate ( see js class MF.runtime.Section).');
+        $container->addCompilerPass($sectionsProvider->createCompilerPass());
 
-        // CSS files to include in a page where the runtime will be bootstrapped
-        $container->addCompilerPass(
-            new CompositeContributorsProviderCompilerPass('modera_mjr_integration.css_resources_provider')
-        );
+        $cssResourcesProvider = new ExtensionPoint('modera_mjr_integration.css_resources_provider');
+        $cssResourcesProvider->setDescription('CSS files to include in a page where the runtime will be bootstrapped');
+        $container->addCompilerPass($cssResourcesProvider->createCompilerPass());
 
-        // allows to add javascript class loader mappings. getItems() must return result which which has the following
-        // structure:
-        // array(
-        //  'foonamespace' => 'bundles/foopath',
-        //  'barnamespace' => 'bundles/barpath'
-        // )
-        //
-        $container->addCompilerPass(
-            new CompositeContributorsProviderCompilerPass('modera_mjr_integration.class_loader_mappings_provider')
+        $classLoaderMappingsProvider = new ExtensionPoint('modera_mjr_integration.class_loader_mappings_provider');
+        $classLoaderMappingsProvider->setDescription(
+            'Allows to add javascript class loader mappings. getItems() must return result which which has the following structure: array("ns" => "path")'
         );
+        $container->addCompilerPass($classLoaderMappingsProvider->createCompilerPass());
 
-        $container->addCompilerPass(
-            new CompositeContributorsProviderCompilerPass('modera_mjr_integration.js_resources_provider')
-        );
+        $jsResourcesProvider = new ExtensionPoint('modera_mjr_integration.js_resources_provider');
+        $jsResourcesProvider->setDescription('Allows to contribute JS resources that will be used in Backend.');
+        $container->addCompilerPass($jsResourcesProvider->createCompilerPass());
     }
 }
