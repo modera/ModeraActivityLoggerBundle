e5aebf85c0c9b81b22c12642abd79962ffc97025
Sergei Lissovski <sergei.lissovski@gmail.com>
2014-05-27 13:28:59 +0300

    [MPFE-609] extension points now are properly defined

--- ModeraBackendDashboardBundle.php
+++ ModeraBackendDashboardBundle.php
@@ -3,6 +3,7 @@
 namespace Modera\BackendDashboardBundle;
 
 use Sli\ExpanderBundle\DependencyInjection\CompositeContributorsProviderCompilerPass;
+use Sli\ExpanderBundle\Ext\ExtensionPoint;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -19,8 +20,9 @@ class ModeraBackendDashboardBundle extends Bundle
      */
     public function build(ContainerBuilder $container)
     {
-        $container->addCompilerPass(
-            new CompositeContributorsProviderCompilerPass('modera_backend_dashboard.dashboard_provider')
-        );
+        $dashboardProvider = new ExtensionPoint('modera_backend_dashboard.dashboard');
+        $dashboardProvider->setDescription('Allows to contribute new dashboard panels to Backend/Dashboard section.');
+
+        $container->addCompilerPass($dashboardProvider->createCompilerPass());
     }
 }
