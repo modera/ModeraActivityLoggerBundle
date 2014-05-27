be226a270ebdfb599faa22a22c54738779f5a7de
Sergei Lissovski <sergei.lissovski@gmail.com>
2014-05-27 13:30:29 +0300

    [MPFE-609] extension points now are properly defined

--- ModeraBackendToolsBundle.php
+++ ModeraBackendToolsBundle.php
@@ -3,6 +3,7 @@
 namespace Modera\BackendToolsBundle;
 
 use Sli\ExpanderBundle\DependencyInjection\CompositeContributorsProviderCompilerPass;
+use Sli\ExpanderBundle\Ext\ExtensionPoint;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -15,8 +16,9 @@ class ModeraBackendToolsBundle extends Bundle
      */
     public function build(ContainerBuilder $container)
     {
-        $container->addCompilerPass(
-            new CompositeContributorsProviderCompilerPass('modera_backend_tools.sections_provider')
-        );
+        $sectionsProvider = new ExtensionPoint('modera_backend_tools.sections');
+        $sectionsProvider->setDescription('Allows to add new sections to Backend/Tools section.');
+
+        $container->addCompilerPass($sectionsProvider->createCompilerPass());
     }
 }
