483f8ead3fb98f345e40a104aaf2f34b6cc4e5cb
Sergei Lissovski <sergei.lissovski@gmail.com>
2014-05-30 14:38:34 +0300

    [MPFE-582] a problem with SettingsListActivity javascript class not receiving activation parameters fixed ( docs updated )

--- Section/SectionInterface.php
+++ Section/SectionInterface.php
@@ -5,13 +5,15 @@ namespace Modera\BackendToolsSettingsBundle\Section;
 /**
  * A section contributed to "Backend / Tools / Settings" must implement this interface.
  *
+ * @see Modera.backend.configutils.runtime.SettingsListActivity javascript class
+ *
  * @author    Sergei Lissovski <sergei.lissovski@modera.org>
  * @copyright 2014 Modera Foundation
  */
 interface SectionInterface
 {
     /**
-     * @return string  A technical ID that will be used in URL
+     * @return string  A technical ID that will be used in URL.
      */
     public function getId();
 
@@ -28,11 +30,20 @@ interface SectionInterface
     public function getGlyph();
 
     /**
-     * @return string  Fully qualified javascript class name of activity class that will represent this section
+     * @return string  Fully qualified javascript class name of activity class that will represent this section.
      */
     public function getActivityClass();
 
     /**
+     * If your javascript activity class needs to accept some activation parameters then you can use 'activationParams'
+     * configuration key in meta information. For example:
+     *
+     *     array(
+     *         'activationParams' => array(
+     *             'category' => 'my-fancy-settings'
+     *         )
+     *     )
+     *
      * @return array Optional metadata this this section may have.
      */
     public function getMeta();
