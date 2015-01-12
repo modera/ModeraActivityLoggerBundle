cf1280fc4b46d6c7a5b99dfea4fe2361f05b08dc
Sergei Vizel <sergei.vizel@modera.net>
2015-01-12 12:52:19 +0200

    [MPFE-688] README.md

--- ModeraMJRThemeIntegrationBundle.php
+++ ModeraMJRThemeIntegrationBundle.php
@@ -4,6 +4,10 @@ namespace Modera\MJRThemeIntegrationBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * @author    Sergei Lissovski <sergei.lissovski@modera.org>
+ * @copyright 2014 Modera Foundation
+ */
 class ModeraMJRThemeIntegrationBundle extends Bundle
 {
 }
