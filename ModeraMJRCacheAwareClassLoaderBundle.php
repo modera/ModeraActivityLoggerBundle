4558c775dca1712d6e68e4f92228b395936136e9
Sergei Vizel <sergei.vizel@modera.net>
2015-01-12 12:45:46 +0200

    [MPFE-688] README.md

--- ModeraMJRCacheAwareClassLoaderBundle.php
+++ ModeraMJRCacheAwareClassLoaderBundle.php
@@ -4,6 +4,10 @@ namespace Modera\MJRCacheAwareClassLoaderBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * @author    Sergei Lissovski <sergei.lissovski@modera.org>
+ * @copyright 2014 Modera Foundation
+ */
 class ModeraMJRCacheAwareClassLoaderBundle extends Bundle
 {
 }
