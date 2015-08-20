f552e327f2c2e36a564903cdd4cfaddcd6b11178
Sergei Lissovski <sergei.lissovski@modera.org>
2015-08-20 10:53:01 +0300

    Session's entity '' is now mapped as 'string' instead of 'blob'. Symfony 2.7 uses Doctrine DBAL 2.4 which doesn't have this type yet

--- Entity/Session.php
+++ Entity/Session.php
@@ -14,8 +14,14 @@ use Doctrine\ORM\Mapping as ORM;
 class Session
 {
     /**
+     * This property mapped column type used to be "binary" with length=128, but in Symfony 2.7 Doctrine DBAL v2.4 is used
+     * and this version doesn't have BinaryType available yet. To solve this we updated mapping to use type "string"
+     * instead, it should be more that enough to store SID, here's the proof:
+     * - http://php.net/manual/en/session.configuration.php#ini.session.hash-function
+     * - http://php.net/manual/en/session.configuration.php#ini.session.hash-bits-per-character
+     *
      * @ORM\Id
-     * @ORM\Column(type="binary", length=128, nullable=false)
+     * @ORM\Column(type="string", nullable=false)
      */
     protected $session_id;
 
