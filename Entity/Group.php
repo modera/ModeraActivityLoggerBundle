2ff3b71b065983c2797d7b1e50177b6fb7931ba8
LiSinCin <crazy.crazy.eco@gmail.com>
2016-04-26 10:06:09 +0300

    added refName normalization function and assert for validator (#10)
    * [MPFE-829] Adding unique constrain to Group:refName. Introducing Group repository method findByRefName
    
    * [MPFE-829] php-cs-fixer code polishing
    
    * [MPFE-829] tests fix
    
    * [MPFE-829] styling fix
    
    * [MPFE-829] styling fix
    
    * [MPFE-829] styling fix
    
    * [MPFE-829] adding refName constriction

--- Entity/Group.php
+++ Entity/Group.php
@@ -39,6 +39,13 @@ class Group
      * Reference name that maybe used in code to refer exact group.
      * Group with ref.name usually will be created through fixtures.
      *
+     * Please note, there is not mandatory Regex assert.
+     * But in modera/backend-security-bindle controller this value will
+     * be normalized by self::normalizeRefNameString
+     *
+     * So if plan to use UI editing of your group, try to stick to this Regex assert.
+     *
+     * @Assert\Regex("/[A-Z_]{0,}/")
      * @ORM\Column(type="string", nullable=true)
      */
     private $refName;
@@ -158,5 +165,12 @@ class Group
     public function setRefName($refName)
     {
         $this->refName = $refName;
+    }
+
+    public static function normalizeRefNameString($proposedRefName)
+    {
+        $modifiedRefName = strtoupper($proposedRefName);
+
+        return preg_replace('/[^A-Z_]+/', '', $modifiedRefName);
     }
 }
