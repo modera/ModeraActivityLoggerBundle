6dcd8e5b99eb6f1f04ba92cce93aa3b1947ecd89
Sergei Lissovski <sergei.lissovski@modera.org>
2016-08-22 14:18:09 +0300

    [MPFE-915][MPFE-918] working prototype allowing to run all tests at once

---
+++ Tests/Fixtures/App/app/ModeraActivityLoggerAppKernel.php
@@ -0,0 +1,18 @@
+<?php
+
+class ModeraActivityLoggerAppKernel extends \Modera\FoundationBundle\Testing\AbstractFunctionalKernel
+{
+    public function registerBundles()
+    {
+        return array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+
+            new Sli\AuxBundle\SliAuxBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Sli\ExtJsIntegrationBundle\SliExtJsIntegrationBundle(),
+
+            new Modera\ActivityLoggerBundle\ModeraActivityLoggerBundle()
+        );
+    }
+}
